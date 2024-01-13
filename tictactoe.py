86ab98e65ba67712208849b6c68fcb4f9df1c376
Nishchaya Roy <nishchayaroy17@gmail.com>
2024-01-13 22:42:19 +0530

    Implemented functions

--- tictactoe.py
+++ tictactoe.py
@@ -22,21 +22,39 @@ def player(board):
     """
     Returns player who has the next turn on a board.
     """
-    raise NotImplementedError
+    x=0
+    o=0
+    for i in range(3):
+        for j in range(3):
+            if board[i][j]==X:
+                x+=1
+            elif board[i][j]==O:
+                o+=1
+    if (x>o):
+        return O
+    elif (x==o):
+        return X
 
 
 def actions(board):
     """
     Returns set of all possible actions (i, j) available on the board.
     """
-    raise NotImplementedError
+    possible_actions=set()
+    for i in range(3):
+        for j in range(3):
+            if board[i][j]==EMPTY:
+                possible_actions.add((i,j))
+    return possible_actions
 
 
 def result(board, action):
     """
     Returns the board that results from making move (i, j) on the board.
     """
-    raise NotImplementedError
+    move=player(board)
+    board[action[0]][action[1]]=move
+    return(board)
 
 
 def winner(board):
@@ -50,18 +68,42 @@ def terminal(board):
     """
     Returns True if game is over, False otherwise.
     """
-    raise NotImplementedError
+    if all(cell != EMPTY for row in board for cell in row) or utility(board) != 0:
+        return True
+    return False
 
 
 def utility(board):
     """
     Returns 1 if X has won the game, -1 if O has won, 0 otherwise.
     """
-    raise NotImplementedError
+    for i in range(3):
+        if board[i][0] == board[i][1] == board[i][2] != EMPTY:
+            if board[i][0]==X:
+                return 1
+            else:
+                return -1
+        if board[0][i] == board[1][i] == board[2][i] != EMPTY:
+            if board[0][i]==X:
+                return 1
+            else:
+                return -1
+    if board[0][0] == board[1][1] == board[2][2] != EMPTY:
+        if board[1][1]==X:
+            return 1
+        else:
+            return -1
+    if board[0][2] == board[1][1] == board[2][0] != EMPTY:
+        if board[1][1]==X:
+            return 1
+        else:
+            return -1
+    return 0
 
 
 def minimax(board):
     """
     Returns the optimal action for the current player on the board.
     """
+    
     raise NotImplementedError
